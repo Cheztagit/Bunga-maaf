af51374ff969834bec7fdb2e75aa12d52345e34f
Chesta <Chestaadabinr@gmail.com>
2026-09-11 20:57:49 +0700

    bunga lebih dikit

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => p.remove(), lifetime);
   }
 
-  for (let i = 0; i < 60; i++) spawnPetal(true);
+  for (let i = 0; i < 35; i++) spawnPetal(true);
   setInterval(() => spawnPetal(false), 100);
 
   /* ------------------------------------------------------------------ *
